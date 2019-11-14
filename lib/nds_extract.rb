3d04f4e5161614c1aaeabf93f8170816e52c32db
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-14 22:56:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(nds)
     gross_total[directors_name] = 0 
     movie_index = 0 
     
-    while movie_index < nds[index][:movies][:worldwide_gross]
+    while movie_index < nds[index][:movies][movie_index]
       
     index += 1 
     end
